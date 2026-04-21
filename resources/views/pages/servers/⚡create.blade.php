270d2b86f74227dd9971c3db220c031013673d60
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2026-04-20 18:04:05 -0600

    Simplify server create page: remove card styling, headings, back button

--- resources/views/pages/servers/⚡create.blade.php
+++ resources/views/pages/servers/⚡create.blade.php
@@ -31,48 +31,20 @@ new #[Title('Add Server')] class extends Component
     }
 }; ?>
 
-<section class="w-full">
-    @include('partials.settings-heading')
+<div class="max-w-xl mx-auto">
+    <flux:input
+        label="{{ __('IP Address') }}"
+        wire:model="ipAddress"
+        placeholder="192.168.1.1"
+        required
+        autofocus
+        data-test="add-server-ip"
+    />
 
-    <flux:heading class="sr-only">{{ __('Add Server') }}</flux:heading>
-
-    <div class="mb-6">
-        <flux:button
-            :href="route('servers.index', [$this->team->slug])"
-            variant="ghost"
-            size="sm"
-            icon="arrow-left"
-            wire:navigate
-        >
-            {{ __('Back to servers') }}
-        </flux:button>
-    </div>
-
-    <flux:heading>{{ __('Add Server') }}</flux:heading>
-    <flux:subheading>{{ $this->team->name }}</flux:subheading>
-
-    <div class="mt-8 max-w-2xl">
-        <div class="rounded-lg border border-zinc-200 bg-white p-6 dark:border-zinc-700 dark:bg-zinc-900">
-            <flux:heading size="lg" class="mb-6">{{ __('Server Details') }}</flux:heading>
-
-            <div class="space-y-6">
-                <flux:input
-                    label="{{ __('IP Address') }}"
-                    wire:model="ipAddress"
-                    placeholder="192.168.1.1"
-                    required
-                    autofocus
-                    data-test="add-server-ip"
-                />
-                <p class="text-sm text-zinc-500 dark:text-zinc-400">{{ __('Enter the IP address of your VPS server') }}</p>
-            </div>
-
-            <div class="mt-6">
-                <flux:button wire:click="create" class="w-full" data-test="add-server-submit">
-                    <span wire:loading.remove>{{ __('Add server') }}</span>
-                    <span wire:loading>{{ __('Adding...') }}</span>
-                </flux:button>
-            </div>
-        </div>
-    </div>
-</section>
+<div class="mt-4 flex">
+    <flux:spacer />
+    <flux:button wire:click="create" variant="primary" data-test="add-server-submit">
+        {{ __('Add server') }}
+    </flux:button>
+</div>
+</div>

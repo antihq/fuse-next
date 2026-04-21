3f710595eb9b8cb4508f6b6f9647d3c0a87cae54
Oliver Servín <7003648+oliverservin@users.noreply.github.com>
2026-04-21 10:46:21 -0600

    Simplify site create page to match minimal server create style

--- resources/views/pages/sites/⚡create.blade.php
+++ resources/views/pages/sites/⚡create.blade.php
@@ -51,54 +51,30 @@ new #[Title('Add Site')] class extends Component
     }
 }; ?>
 
-<section class="w-full">
-    @include('partials.settings-heading')
+<div class="max-w-xl mx-auto">
+    <flux:input
+        label="{{ __('Domain') }}"
+        wire:model="domain"
+        placeholder="example.com"
+        required
+        autofocus
+        data-test="add-site-domain"
+    />
 
-    <flux:heading class="sr-only">{{ __('Add Site') }}</flux:heading>
+    <div class="mt-4">
+        <flux:input
+            label="{{ __('GitHub Repository') }}"
+            wire:model="repository"
+            placeholder="https://github.com/user/repo.git"
+            required
+            data-test="add-site-repository"
+        />
+    </div>
 
-    <div class="mb-6">
-        <flux:button
-            :href="route('sites.index', [$this->team->slug, $this->server])"
-            variant="ghost"
-            size="sm"
-            icon="arrow-left"
-            wire:navigate
-        >
-            {{ __('Back to sites') }}
+    <div class="mt-4 flex">
+        <flux:spacer />
+        <flux:button wire:click="create" variant="primary" data-test="add-site-submit">
+            {{ __('Add site') }}
         </flux:button>
     </div>
-
-    <flux:heading>{{ __('Add Site') }}</flux:heading>
-    <flux:subheading>{{ $server->name }}</flux:subheading>
-
-    <div class="mt-8 max-w-2xl">
-        <div class="rounded-lg border border-zinc-200 bg-white p-6 dark:border-zinc-700 dark:bg-zinc-900">
-            <flux:heading size="lg" class="mb-6">{{ __('Site Details') }}</flux:heading>
-
-            <div class="space-y-6">
-                <flux:input
-                    label="{{ __('Domain') }}"
-                    wire:model="domain"
-                    placeholder="example.com"
-                    required
-                />
-                <p class="text-sm text-zinc-500 dark:text-zinc-400">{{ __('The domain name for your site (e.g., example.com)') }}</p>
-
-                <flux:input
-                    label="{{ __('GitHub Repository') }}"
-                    wire:model="repository"
-                    placeholder="https://github.com/user/repo.git"
-                    required
-                />
-                <p class="text-sm text-zinc-500 dark:text-zinc-400">{{ __('The GitHub repository URL to deploy') }}</p>
-            </div>
-
-            <div class="mt-6">
-                <flux:button wire:click="create" class="w-full">
-                    <span wire:loading.remove>{{ __('Create site') }}</span>
-                    <span wire:loading>{{ __('Creating...') }}</span>
-                </flux:button>
-            </div>
-        </div>
-    </div>
-</section>
+</div>
